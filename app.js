000438d00f1d4f5037793cdd6976a06fae587f91
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-05-30 14:17:09 -0500

    add config, ODM mapper, validator and connect database

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose'); //ODM (Object Document Mapper)
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Test the dev config file //remove for production
+var config = require('./config.dev');
+//Connect to MongoDB
+mongoose.connect(config.mongodb, { useNewUrlParser: true });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -37,5 +41,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//Test the dev config file
+//remove for production
+console.log(config); //cli npm start reveals JSON mongodb connection
 
 module.exports = app;
