1c6d45b4389e3b3a6a880ecc6b5c06456cf55121
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-05-31 13:54:16 -0500

    added endpoint for user registration

---
+++ routes/api/auth.js
@@ -0,0 +1,75 @@
+//Register a new user
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+
+var Users = require('../../models/users');
+
+router.post('/register', function(req,res,next){
+  var data = req.body;
+
+  Users.register(new Users({
+    username: data.username,
+    email: data.email,
+    first_name: data.first_name,
+    last_name: data.last_name
+  }),
+  data.password,
+  function(err, user){
+
+    if(err){
+
+      return res.json({
+        success: false,
+        user: req.body,
+        errors: err
+      });
+
+    }
+
+    return res.json({
+      success: true,
+      user: user
+    });
+
+  });
+
+});
+
+// router.post('/login', function(req, res, next) {
+  
+//   passport.authenticate('local', function(err, user, info) {
+
+//     if (err) {
+//       return res.json({success:false, error: err});
+//     }
+
+//     if (!user) {
+//       return res.json({success:false, error: info.message });
+//     }
+
+//     req.logIn(user, function(err) {
+
+//       if (err) {
+//         return res.json({success:false, error: err });
+//       }
+
+//       //we will use a console.log() to test the session data
+//       console.log(req.session);
+
+//       return res.json({success:true, user: user });
+//     });
+//   })(req, res, next);
+// });
+
+// router.delete('/logout', function(req, res){
+//   req.logout();
+//   // if(!req.session.passport.user){
+//   if(!req.session.passport){
+//     return res.json({success: 'true'});
+//   }else{
+//     return res.json({success: 'false'});
+//   }
+// });
+
+module.exports = router;
