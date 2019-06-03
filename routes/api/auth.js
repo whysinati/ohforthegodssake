6896d71ba8b1b1ee7cfcdc4b1c9e278a9d1f3b10
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-06-03 15:33:51 -0500

    added API and non-API logout methods

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -62,15 +62,18 @@ router.post('/login', function(req, res, next) {
   })(req, res, next);
 });
 
-// router.delete('/logout', function(req, res){
-router.get('/logout', function(req, res){
+router.delete('/logout', function(req, res){
+//router.get('/logout', function(req, res){ //this was the initial logout function
+  //navigate to http://localhost:3001/api/auth/logout to test console log statements
+  //console.log(req.session); //shows cookie and passport (in terminal window) before logout
   req.logout();
-//   // if(!req.session.passport.user){
-//   if(!req.session.passport){
-//     return res.json({success: 'true'});
-//   }else{
-//     return res.json({success: 'false'});
-//   }
+  //console.log(req.session); //shows cookie and passport (in terminal window) after logout
+//   // if(!req.session.passport.user){ //caused errors in other sites using this api
+  if(!req.session.passport){
+    return res.json({success: 'true'});
+  }else{
+    return res.json({success: 'false'});
+  }
 });
 
 module.exports = router;
--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('auth/index', { title: 'Authenticate User' });
 });
-// router.get('/logout', function(req, res){
-//   req.logout();
-//   res.redirect('/auth');
-// });
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/auth');
+  //res.redirect('/'); //this will go back to home, based on current settings
+});
 module.exports = router;
