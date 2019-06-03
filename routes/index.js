13ce86c4a80f0f51ac9780241ec5e9bb51ed99cd
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-06-03 14:13:56 -0500

    built out login feature

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //console.log(req.session); //shows cookie & passport object in terminal window
   res.render('index', { title: 'Gram\'s 100th Birthday!' });
 });
 
--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -26,9 +26,43 @@ var authApp = (function() {
         app.innerHTML=form;
     }
 
+    function postRequest(formId, url){
+        let form = document.getElementById(formId);
+        form.addEventListener('submit', function(e){
+          e.preventDefault();
+      
+          let formData = new FormData(form);
+          let uri = `${window.location.origin}${url}`;
+          let xhr = new XMLHttpRequest();
+          xhr.open('POST', uri);
+      
+          xhr.setRequestHeader(
+            'Content-Type',
+            'application/json; charset=UTF-8'
+          );
+      
+          let object = {};
+          formData.forEach(function(value, key){
+            object[key]=value;
+          });
+      
+          xhr.send(JSON.stringify(object));
+          xhr.onload = function(){
+            let data = JSON.parse(xhr.response);
+            //console.log(data); //shows login form entries submitted
+            if(data.success===true){
+                window.location.href = '/';
+            }else{
+                document.getElementById('formMsg').style.display='block';
+            }
+          }
+        });
+    }
+
     return {
         load: function(){
             loginForm();
+            postRequest('loginForm', '/api/auth/login');
         }
       }
 
